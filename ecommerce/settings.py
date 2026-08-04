e1b5ccbae4c62367ee862e5f68f749e7bc296c0b
yafiathana-max <yafiathana555@gmail.com>
2026-08-04 20:30:04 +0530

    Fixed static files for production

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,6 +129,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/'
