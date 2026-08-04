cd4ab83d78b1215b41ad314f4acea6e497300075
yafiathana-max <yafiathana555@gmail.com>
2026-08-04 21:30:06 +0530

    Add automatic superuser creation

---
+++ create_superuser.py
@@ -0,0 +1,21 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
+django.setup()
+
+from django.contrib.auth.models import User
+
+username = "adminuser09"
+email = "adminuser09@gmail.com"
+password = "adminuser09"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username,
+        email,
+        password
+    )
+    print("Superuser created")
+else:
+    print("Superuser already exists")
