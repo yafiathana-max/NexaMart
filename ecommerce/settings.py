8c4b2fea6a3ddf92c2904ab3a24be7c8f8f3e9b3
yafiathana-max <yafiathana555@gmail.com>
2026-08-04 20:26:58 +0530

    Configure production settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b^xf&z(sx@d_0nmab8o)suiz(cnq)0wg++@os%)lal(c4=5h5%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "nexamart-zyzw.onrender.com",
+]
 
 
 # Application definition
