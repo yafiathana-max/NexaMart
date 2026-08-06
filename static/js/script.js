2871fbc883fc0769b8f92f6e35dcd7258ac13ee8
yafiathana-max <yafiathana555@gmail.com>
2026-08-06 21:23:58 +0530

    Fix mobile navbar hamburger menu

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
-    const menu = document.getElementById("navLinks");
-
+    const menu = document.getElementById("nav-links");
     menu.classList.toggle("active");
 }
+
+window.toggleMenu = toggleMenu;
